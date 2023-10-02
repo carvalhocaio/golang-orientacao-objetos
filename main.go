25d4454e50279e6c75b338dc5bee35f730a50384
carvalhocaio <caiocarvalho.py@gmail.com>
2023-10-02 16:50:31 -0400

    transferencia entre contas

--- main.go
+++ main.go
@@ -29,12 +29,27 @@ func (cc *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	}
 }
 
-func main() {
-	contaMartin := ContaCorrente{}
-	contaMartin.titular = "Martin"
-	contaMartin.saldo = 500
+func (cc *ContaCorrente) Tranferir(
+	valorDaTransferencia float64,
+	contaDestino *ContaCorrente,
+) bool {
+	if valorDaTransferencia < cc.saldo && valorDaTransferencia > 0 {
+		cc.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
 
-	fmt.Println(contaMartin.saldo)
-	fmt.Println(contaMartin.Depositar(200))
-	fmt.Println(contaMartin.saldo)
+func main() {
+	contaMartin := ContaCorrente{titular: "Martin", saldo: 300}
+	contaHailey := ContaCorrente{titular: "Hailey", saldo: 100}
+
+	status := contaMartin.Tranferir(150, &contaHailey)
+
+	fmt.Println(status)
+
+	fmt.Println(contaMartin)
+	fmt.Println(contaHailey)
 }
